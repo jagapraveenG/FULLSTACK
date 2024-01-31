3198e15c02a5acafeabe0a644bbfbeb8574d6586
jagapraveenG <125950508+jagapraveenG@users.noreply.github.com>
2024-01-31 14:53:09 +0530

    Add files via upload

---
+++ day5/src/components/Booking/BoatBookingDetails.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import Navbar from "../Nabar";
+import "./BoatDetail.css";
+
+const BoatDetails = () => {
+    return(
+        <div>
+            <header>
+                <Navbar/>
+            </header>
+        <>
+            
+            <div className="container">
+                <h1 className="title">Boat Details</h1>
+            </div>
+            <div className="details-container">
+                <div className="image-container">
+                    <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2a/e6/45/bd/caption.jpg?w=500&h=400&s=1" alt="Boat" className="image" />
+                    <section className="sale">
+                        <span className="sale-label">For Sale</span>
+                        <span className="price-value">$540,000</span>
+                    </section>
+                </div>
+                <div className="details">
+                    <div className="details-content">
+                        <h2>Boat Details</h2>
+                        <p>This luxurious boat is perfect for leisurely cruises along the coast. With its spacious cabins and modern amenities, it offers a comfortable and enjoyable experience for all passengers.</p>
+                        <p>Located at XYZ Boatyard, Bangalore</p>
+                    </div>
+                    <aside>
+                        <h2>About This Boat</h2>
+                        <p>This luxurious boat is perfect for leisurely cruises along the coast. With its spacious cabins and modern amenities, it offers a comfortable and enjoyable experience for all passengers.</p>
+                        <h2>Safety Requirements</h2>
+                        <ul>
+                            <li>Life jackets provided for all passengers</li>
+                            <li>First aid kit onboard</li>
+                            <li>Experienced captain and crew</li>
+                        </ul>
+                    </aside>
+                </div>
+            </div>
+            <div className="button-container">
+                <Link to="/buy-boat" className="button-link">
+                    <button className="book-button">
+                        <FontAwesomeIcon icon={faShoppingCart} /> Book
+                    </button>
+                </Link>
+            </div>
+            <div className="owner-details">
+                <h2>Owner Details</h2>
+                <p>Owned by John Doe</p>
+                <p>Contact: johndoe@example.com</p>
+            </div>
+            
+        </>
+        </div>
+    )
+}
+
+export default BoatDetails;
