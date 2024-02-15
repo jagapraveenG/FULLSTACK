d0863fa7e455a671e6fdfa47c918bc854c9a10a2
jagapraveenG <125950508+jagapraveenG@users.noreply.github.com>
2024-02-15 12:56:14 +0530

    Add files via upload

---
+++ backend/jagan/src/main/java/jagan/jagan/JaganApplication.java
@@ -0,0 +1,13 @@
+package jagan.jagan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaganApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaganApplication.class, args);
+	}
+
+}
